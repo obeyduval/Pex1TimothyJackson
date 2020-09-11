87a9f9b322868582ec46c1bd9e84da7f1236fb5f
obeyduval <C22Timothy.Jackson@usafa.edu>
2020-09-11 09:22:54 -0600

    vector calculator function initalized and printed out x and y variables to check

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,17 @@ public class Main {
     public static void main(String[] args) {
 	    // introduce user to calculator UI
         System.out.println("Welcome to the Vector Calculator!");
+
+        //initialize input double variables
+        double x = 0.0, y = 0.0;
+
+        //main vector calculator function
+        vectorCalculator(x,y);
     }
+
+    public static void vectorCalculator( double x, double y){
+        System.out.println("x is : " + x);
+        System.out.println("y is : " + y);
+    }
+
 }
