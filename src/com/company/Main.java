7b765f173e85e450bf9c4903014bf13ba4239809
obeyduval <C22Timothy.Jackson@usafa.edu>
2020-09-11 09:18:19 -0600

    Documentation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,9 @@
 package com.company;
-
+/*
+* @authors - Timothy Jackson
+* @teacher - Dr. Hadfield
+* @pex - PEX 1 - Vector Calculator
+ */
 public class Main {
 
     public static void main(String[] args) {
