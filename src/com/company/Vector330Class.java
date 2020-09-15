68c31742a7783891e73248d1b9e374d77ced3b25
obeyduval <C22Timothy.Jackson@usafa.edu>
2020-09-15 15:03:58 -0600

    version 2.0

--- src/com/company/Vector330Class.java
+++ src/com/company/Vector330Class.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public class Vector330Class {
-    private static double EPS = 1.0E-9;
+    private static final double EPS = 1.0E-9;
     private double x;
     private double y;
 
@@ -40,7 +40,7 @@ public class Vector330Class {
         returns y component of vector as a double
      */
     public double getY(){
-        return (double) this.x;
+        return (double) this.y;
     }
     /*
         returns x component of vector as a int
@@ -88,28 +88,28 @@ public class Vector330Class {
      sets the x component of vector using an input of type int
     */
     public void setX(int x){
-        this.x = x;
+        this.x = (double) x;
     }
 
     /*
      sets the y component of vector using an input of type int
     */
     public void setY(int y){
-        this.y = y;
+        this.y = (double) y;
     }
 
     /*
      sets the x component of vector using an input of type long
     */
     public void setX(long x){
-        this.x = x;
+        this.x = (double) x;
     }
 
     /*
      sets the y component of vector using an input of type long
     */
     public void setY(long y){
-        this.y = y;
+        this.y = (double) y;
     }
 
     /*
@@ -118,22 +118,22 @@ public class Vector330Class {
     components within epsilon of each other
      */
     public boolean equals(Vector330Class v){
-        return v == null;
+
+        return (Math.abs(this.x - v.x) < EPS ) && (Math.abs(this.y - v.y) < EPS);
     }
 
     /*
     does vector addition of this vector with the one passed in
+    Vector330Class res = a.add(b);
      */
     public Vector330Class add(Vector330Class v){
         //vector addition
-
-        return v;
+        return new Vector330Class(this.x + v.x, this.y + v.y);
     }
 
-    /*
+    /**
     subtracts passed in vector from this vector
      */
-
     public Vector330Class subtract(Vector330Class v){
         return v;
     }
@@ -142,59 +142,59 @@ public class Vector330Class {
      computes the dot product of this vector and the other vector
      */
 
-    public double dotProduct(Vector330Class v){
-        return 0.0;
-    }
-
-    /*
-    does a scalar-vector multiplication of this vector with double value passed in
-     */
-
-    public Vector330Class scale( double s) {
-        //return;
-    }
-
-    /*
-    computes the magnitude (2-norm or length) of this vector
-     */
-
-    public double magnitude(){
-
-        return 0.0;
-    }
-
-    /*
-    computes and returns the direction (orientation) of the vector in radians
-     */
-
-    public double direction(){
-        return 0.0;
-    }
-
-    /*
-    creates a normalized (of length one) vector in same direction as this vector
-     */
-
-    public Vector330Class normalize(){
-        //return;
-    }
-
-    /*
-    overrides the default toString() method producing an angle-bracket version of this vector
-     */
-
-    public java.lang.String toString(){
-        //return;
-    }
-
-    /*
-     inputs a Scanner object from which it reads and parses a string representing the
-     vector with and expected form of "< 3.0, 4.0 >" - note spaces are needed after '<',
-     after the comma, and before the '>'.
-     */
-    public static Vector330Class parseVector(java.util.Scanner s) throws java.lang.Exception{
-        //return;
-    }
+//    public double dotProduct(Vector330Class v){
+//        return 0.0;
+//    }
+//
+//    /*
+//    does a scalar-vector multiplication of this vector with double value passed in
+//     */
+//
+//    public Vector330Class scale( double s) {
+//        //return;
+//    }
+//
+//    /*
+//    computes the magnitude (2-norm or length) of this vector
+//     */
+//
+//    public double magnitude(){
+//
+//        return 0.0;
+//    }
+//
+//    /*
+//    computes and returns the direction (orientation) of the vector in radians
+//     */
+//
+//    public double direction(){
+//        return 0.0;
+//    }
+//
+//    /*
+//    creates a normalized (of length one) vector in same direction as this vector
+//     */
+//
+//    public Vector330Class normalize(){
+//        //return;
+//    }
+//
+//    /*
+//    overrides the default toString() method producing an angle-bracket version of this vector
+//     */
+//
+//    public java.lang.String toString(){
+//        //return;
+//    }
+//
+//    /*
+//     inputs a Scanner object from which it reads and parses a string representing the
+//     vector with and expected form of "< 3.0, 4.0 >" - note spaces are needed after '<',
+//     after the comma, and before the '>'.
+//     */
+//    public static Vector330Class parseVector(java.util.Scanner s) throws java.lang.Exception{
+//        //return;
+//    }
 }
 
 
--- src/com/company/Vector330ClassTest.java
+++ src/com/company/Vector330ClassTest.java
@@ -1,5 +1,73 @@
 package com.company;
 
 public class Vector330ClassTest {
+    private static final double EPS = 1.0E-9;
+    Vector330Class a;
+    Vector330Class b;
 
+    /**@BeforeEach
+     *
+     */
+    void setUp(){
+
+    }
+
+    /**@AfterEach
+     *
+     */
+    void tearDown(){
+
+    }
+
+    /**@Test
+     *
+     */
+    void getX(){
+
+    }
+
+    /**@Test
+     *
+     */
+    void getY(){
+
+    }
+
+    /**@Test
+     *
+     */
+    void getXint(){
+
+    }
+
+    /**@Test
+     *
+     */
+    void getYint(){
+
+    }
+
+    /**@Test
+     *
+     */
+    void getXlong(){
+
+    }
+
+    /**@Test
+     *
+     */
+    void getYlong(){
+
+    }
+
+    /**@Test
+     *
+     */
+    void setX(){
+
+    }
+
+    /**
 }
+
